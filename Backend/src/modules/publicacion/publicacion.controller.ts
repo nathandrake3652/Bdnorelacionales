bbea7f1dc85881eea788f5a5d0630d26c128f34a
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-07-02 11:47:55 -0400

    votar hook

--- Backend/src/modules/publicacion/publicacion.controller.ts
+++ Backend/src/modules/publicacion/publicacion.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PublicacionService } from './publicacion.service';
 import { CreatePublicacionDto } from './dto/create-publicacion.dto';
 import { UpdatePublicacionDto } from './dto/update-publicacion.dto';
+import { VotarDto } from './dto/votar.dto';
 
 @Controller('publicacion')
 export class PublicacionController {
@@ -46,6 +47,10 @@ async publicacionesPorEtiquetaYFiltro(
 ) {
   return this.publicacionService.getPorEtiquetaYFiltro(etiqueta, filtro);
 }
+@Patch('votar')
+async votarPublicacion(@Body() votarDto: VotarDto) {
+  return this.publicacionService.votar(votarDto);
 
 
+}
 }
